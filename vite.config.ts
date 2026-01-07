274360a448f784f89274185e40ee75436e1753b3
ThanhDat7A <pn9055162@gmail.com>
2026-01-07 19:56:31 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-       base: '/Pic2Word-Analyst/',  
+       base: '/app-chuyendoind/',  
       server: {
         port: 3000,
         host: '0.0.0.0',
